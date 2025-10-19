0cbb0f980dde94086153f306f03607a9ec4990cc
dotmantosh <dotuntosh@gmail.com>
2025-10-19 02:48:41 +0100

    name changed

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100 admin-login admin-container">
-      <h1 className="text-4xl font-bold mb-4">Welcome to OBMS CBT</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Tosh Tech CBT</h1>
       <p className="text-lg text-gray-700">Please click the button below to get started.</p>
         <button onClick={()=>navigate('/student')}>Get Started</button>
     </div>
